45f247f85594e42e43dcba0b40c481565f98eb3a
peachbranch7 <peachbranch.0517@gmail.com>
2022-09-18 17:48:54 +0900

    fix: 🐛 clean up

--- src/components/UserList/index.tsx
+++ src/components/UserList/index.tsx
@@ -7,16 +7,16 @@ export type User = {
 };
 
 type Props = {
-  user: User;
+  user?: User;
 };
 
 export const UserList: FC<Props> = (props) => {
   const { user } = props;
   return (
-    <ul key={user.id}>
-      <li>id: {user.id}</li>
-      <li>name: {user.name}</li>
-      <li>age: {user.age}</li>
+    <ul key={user?.id}>
+      <li>id: {user?.id}</li>
+      <li>name: {user?.name}</li>
+      <li>age: {user?.age}</li>
     </ul>
   );
 };
--- src/stories/UserList/index.stories.tsx
+++ src/stories/UserList/index.stories.tsx
@@ -1,5 +1,7 @@
 import type { ComponentMeta, ComponentStoryObj } from '@storybook/react';
+import { rest } from 'msw';
 import { UserList } from '../../components/UserList';
+import { handlers } from '../../mocks/handlers';
 
 export default {
   title: 'UserList',
@@ -13,5 +15,30 @@ export const Default: ComponentStoryObj<typeof UserList> = {
       name: 'james',
       age: 24,
     },
+  },
+};
+
+export const SuccessBehavior = () => <UserList />;
+
+SuccessBehavior.parameters = {
+  // 正常系は定義済みのhandlersをそのまま利用
+  msw: {
+    handlers: handlers,
+  },
+};
+
+export const FailureBehavior = () => <UserList />;
+
+FailureBehavior.parameters = {
+  // 異常系はhandlersを上書き
+  msw: {
+    handlers: [
+      rest.get('/api/users', (_, res, ctx) => {
+        return res(
+          ctx.status(500),
+          ctx.json({ message: 'Internal Server Error' })
+        );
+      }),
+    ],
   },
 };
